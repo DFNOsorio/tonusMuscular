ccf8fd69864bca53fced89c0c201092f3822e1c0
Rita <arc.mendes@campus.fct.unl.pt>
2017-02-07 14:32:03 +0000

    Initial commit

--- segmentator.py
+++ segmentator.py
@@ -5,15 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-filename1 = 'Testes_Egas_Moniz/patient_1_mvc_0007803B4638_2016-11-28_12-28-21.h5'
-filename2 = 'Testes_Egas_Moniz/patient_1_plataforma_0007803B4638_2016-11-28_12-37-24.h5'
-filename3 = 'Testes_Egas_Moniz/patient_1_plataforma_0007803B4638_2016-11-28_12-41-14.h5'
-filename4 = 'Testes_Egas_Moniz/patient_1_plataforma_0007803B4638_2016-11-28_12-43-17.h5'
-filename5 = 'Testes_Egas_Moniz/patient_1_plataforma_000780646DDA_0007803B4638_2016-11-28_12-33-04.h5'
-filename6 = 'Testes_Egas_Moniz/patient_1_repouso_0007803B4638_2016-11-28_12-27-00.h5'
+filename1 = 'Testes_plat/patient_2_mvc_0007803B4638_2016-12-02_16-55-49.h5'
+filename2 = 'Testes_plat/patient_2_plataforma_000780646DDA_0007803B4638_2016-12-02_16-58-41.h5'
+filename3 = 'Testes_plat/patient_2_plataforma_000780646DDA_0007803B4638_2016-12-02_16-59-47.h5'
+filename4 = 'Testes_plat/patient_2_repouso_0007803B4638_2016-12-02_16-54-50.h5'
 
 
-filename_w='Egas_Moniz_Segments/Paciente1_MJ_Lupus.h5'
+"""
+if("plataforma in filename#):
+    platform == True
+
+"""
+filename_w='Egas_Moniz_Segments/Paciente2_Emma_Healthy.h5'
 
 
 def load_data_h5(filename, platform = False):
@@ -94,25 +97,29 @@ def load_data(segment_data, segment):
             f_new[segment].create_dataset('channel'+ str(channel), data=segment_data[:, x])
             x += 1
 
+def load_data_plat(segment_data, segment):
+    for x, y in enumerate(segment_data):
+        if x<=3:
+            channel=x+1
+            f_new[segment].create_dataset('channel'+ str(channel), data=segment_data[:, x])
+            x += 1
 
-
-rest_time, rest_EMG_data, rest_EMG_lables       = load_data_h5(filename6)
+rest_time, rest_EMG_data, rest_EMG_lables       = load_data_h5(filename4)
 
 MVC1_time, MVC1_EMG_data, MVC1_EMG_lables       = load_data_h5(filename1)
 
-plat1_time, plat1_EMG_data, plat1_EMG_lables    = load_data_h5(filename2)
-plat2_time, plat2_EMG_data, plat2_EMG_lables    = load_data_h5(filename3)
-plat3_time, plat3_EMG_data, plat3_EMG_lables    = load_data_h5(filename4)
-plat4_time, plat4_EMG_data, plat4_EMG_lables    = load_data_h5(filename5)
+plat1_time, plat1_EMG_data, plat1_EMG_lables, plat1_data, plat1_lables    = load_data_h5(filename2, platform=True)
+plat2_time, plat2_EMG_data, plat2_EMG_lables, plat2_data, plat2_lables    = load_data_h5(filename3, platform=True)
 
-plt.plot(plat3_EMG_data)
-plt.show()
+#plt.plot(MVC1_EMG_data)
+#plt.show()
+
 f_new = h5py.File(filename_w, 'w')
 
-atrib1 = f_new.attrs['Name'] = 'M.J.'
+atrib1 = f_new.attrs['Name'] = 'E.R.'
 atrib2 = f_new.attrs['Gender'] = 'F'
-atrib3 = f_new.attrs['Age'] = 54
-atrib4 = f_new.attrs['Condition'] = 'Lupus'
+atrib3 = f_new.attrs['Age'] = 21
+atrib4 = f_new.attrs['Condition'] = 'Healthy'
 atrib5 = f_new.attrs ['Channel1'] = 'RA_L'
 atrib6 = f_new.attrs ['Channel2'] = 'RA_R'
 atrib7 = f_new.attrs ['Channel3'] = 'O_L'
@@ -151,61 +158,70 @@ f_new.create_group('Platform/Reach_L')
 f_new.create_group('Platform/Reach_C')
 f_new.create_group('Platform/Reach_Ground')
 
-sliceMVC1=1.000
-slice_2_MVC1= 14.399
-#sliceMVC2=20.0
-#slice_2_MVC2= 30.149
+sliceMVC1=15
+slice_2_MVC1=35
+sliceMVC2=46
+slice_2_MVC2= 66
 
-slice_AE=70.000
-slice_2_AE=85.000
+slice_AE=96
+slice_2_AE=126
+slice_2FS=124
+slice_2_2FS=154
+slice_2FS_EC=164
+slice_2_2FS_EC=194
+slice_RF_EO=217
+slice_2_RF_EO=247
+slice_RF_EC=263
+slice_2_RF_EC=333
+slice_LF_EO = 347
+slice_2_LF_EO=377
+slice_LF_EC = 388
+slice_LF2_EC = 418
+slice_reach_R = 490
+slice_reach2_R = 496
+slice_reach_L = 508
+slice_reach2_L = 514
+slice_reach_C =524
+slice_reach2_C = 530
+slice_reach_G = 567
+slice_reach2_G = 575.399
 
-slice_2FS=0.0
-slice_2_2FS=30.0
-slice_2FS_EC=37.0
-slice_2_2FS_EC=67.0
-slice_RF_EO=87.0
-slice_2_RF_EO=117.0
-slice_RF_EC=161.0
-slice_2_RF_EC=191.0
-slice_LF_EO = 177.0
-slice_2_LF_EO= 207.0
-
-slice_LF_EC = 12.0
-slice_LF2_EC = 60.0
-slice_reach_R = 65.0
-slice_reach2_R = 71.0
-slice_reach_L = 80.0
-slice_reach2_L = 86.0
-slice_reach_C =90
-slice_reach2_C = 96
-
-slice_reach_G = 6.0
-slice_reach2_G = 12.0
-
-slice_repouso=4.0
-slice_2_repouso = 33.149
+slice_repouso=0.0
+slice_2_repouso = 17.849
 
 rest_segment = segments(rest_time, rest_EMG_data, slice_repouso, slice_2_repouso)
 
 MVC1_segment = segments(MVC1_time, MVC1_EMG_data, sliceMVC1, slice_2_MVC1)
-#MVC2_segment = segments(MVC1_time, MVC1_EMG_data, sliceMVC2, slice_2_MVC2)
+MVC2_segment = segments(MVC1_time, MVC1_EMG_data, sliceMVC2, slice_2_MVC2)
 
-AE_segment     = segments(plat4_time, plat4_EMG_data, slice_AE, slice_2_AE)
-FS_segment    = segments(plat1_time, plat1_EMG_data, slice_2FS, slice_2_2FS)
-FS_EC_segment = segments(plat1_time, plat1_EMG_data, slice_2FS_EC, slice_2_2FS_EC)
-RF_EO_segment  = segments(plat1_time, plat1_EMG_data, slice_RF_EO, slice_2_RF_EO)
-RF_EC_segment  = segments(plat1_time, plat1_EMG_data, slice_RF_EC, slice_2_RF_EC)
-LF_EO_segment = segments(plat1_time, plat1_EMG_data, slice_LF_EO, slice_2_LF_EO)
+AE_segment     = segments(plat2_time, plat2_EMG_data, slice_AE, slice_2_AE)
+FS_segment    = segments(plat2_time, plat2_EMG_data, slice_2FS, slice_2_2FS)
+FS_EC_segment = segments(plat2_time, plat2_EMG_data, slice_2FS_EC, slice_2_2FS_EC)
+RF_EO_segment  = segments(plat2_time, plat2_EMG_data, slice_RF_EO, slice_2_RF_EO)
+RF_EC_segment  = segments(plat2_time, plat2_EMG_data, slice_RF_EC, slice_2_RF_EC)
+LF_EO_segment = segments(plat2_time, plat2_EMG_data, slice_LF_EO, slice_2_LF_EO)
 LF_EC_segment = segments(plat2_time, plat2_EMG_data, slice_LF_EC, slice_LF2_EC)
 reach_R_segment = segments(plat2_time, plat2_EMG_data, slice_reach_R, slice_reach2_R)
 reach_L_segment = segments(plat2_time, plat2_EMG_data, slice_reach_L, slice_reach2_L)
 reach_C_segment = segments(plat2_time, plat2_EMG_data, slice_reach_C, slice_reach2_C)
-reach_G_segment = segments(plat3_time, plat3_EMG_data, slice_reach_G, slice_reach2_G)
+reach_G_segment = segments(plat2_time, plat2_EMG_data, slice_reach_G, slice_reach2_G)
+
+AE_plat_segment     = segments(plat2_time, plat2_data, slice_AE, slice_2_AE)
+FS_plat_segment    = segments(plat2_time, plat2_data, slice_2FS, slice_2_2FS)
+FS_EC_plat_segment = segments(plat2_time, plat2_data, slice_2FS_EC, slice_2_2FS_EC)
+RF_EO_plat_segment  = segments(plat2_time, plat2_data, slice_RF_EO, slice_2_RF_EO)
+RF_EC_plat_segment  = segments(plat2_time, plat2_data, slice_RF_EC, slice_2_RF_EC)
+LF_EO_plat_segment = segments(plat2_time, plat2_data, slice_LF_EO, slice_2_LF_EO)
+LF_EC_plat_segment = segments(plat2_time, plat2_data, slice_LF_EC, slice_LF2_EC)
+reach_R_plat_segment = segments(plat2_time, plat2_data, slice_reach_R, slice_reach2_R)
+reach_L_plat_segment = segments(plat2_time, plat2_data, slice_reach_L, slice_reach2_L)
+reach_C_plat_segment = segments(plat2_time, plat2_data, slice_reach_C, slice_reach2_C)
+reach_G_plat_segment = segments(plat2_time, plat2_data, slice_reach_G, slice_reach2_G)
 
 
 load_data(rest_segment, 'Static/Relax')
 load_data(MVC1_segment, "Static/MVC1")
-#load_data(MVC2_segment, "Static/MVC2")
+load_data(MVC2_segment, "Static/MVC2")
 
 load_data(AE_segment, "EMG/Arms_extension")
 load_data(FS_segment, "EMG/Standing_EO")
@@ -218,4 +234,17 @@ load_data(reach_R_segment, "EMG/Reach_R")
 load_data(reach_L_segment, "EMG/Reach_L")
 load_data(reach_G_segment, "EMG/Reach_Ground")
 load_data(reach_C_segment, "EMG/Reach_C")
+
+load_data_plat(AE_plat_segment, "Platform/Arms_extension")
+load_data_plat(FS_plat_segment, "Platform/Standing_EO")
+load_data_plat(FS_EC_plat_segment, "Platform/Standing_EC")
+load_data_plat(RF_EO_plat_segment, "Platform/OneFootStanding_R_EO")
+load_data_plat(RF_EC_plat_segment, "Platform/OneFootStanding_R_EC")
+load_data_plat(LF_EO_plat_segment, "Platform/OneFootStanding_L_EO")
+load_data_plat(LF_EC_plat_segment, "Platform/OneFootStanding_L_EC")
+load_data_plat(reach_R_plat_segment, "Platform/Reach_R")
+load_data_plat(reach_L_plat_segment, "Platform/Reach_L")
+load_data_plat(reach_G_plat_segment, "Platform/Reach_Ground")
+load_data_plat(reach_C_plat_segment, "Platform/Reach_C")
+
 ### Faltam os outros
