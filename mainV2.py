4979c400ba090a035d1a4df9d3d06e849653f828
Rita <arc.mendes@campus.fct.unl.pt>
2017-02-16 16:47:44 +0000

    plot

--- mainV2.py
+++ mainV2.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib.backends.backend_pdf import PdfPages
 
-file = "Egas_Moniz_Segments/Paciente1_Ines_Healthy.h5"
+file = "Egas_Moniz_Segments/Paciente1_MJ_Lupus.h5"
 
-patient1 = Patient(file, platform=True, verbose=True)
+patient1 = Patient(file, platform=False, verbose=True)
 
 patient1.static_avg = avg_out(patient1.static)
 patient1.EMG_avg = avg_out(patient1.EMG)
@@ -23,12 +23,13 @@ patient1.static_normalization, patient1.static_max_values = norm_whole_segment(p
 patient1.EMG_normalization, patient1.EMG_max_values = norm_whole_segment(patient1.EMG_RMS, patient1.RMS_max)
 print '\033[93m' + "NORM_END" + '\033[0m'
 
-patient1.platformdata = RAW_2_mass(patient1.platform)
-patient1.COP = mass_2_COP(patient1.platformdata)
+#patient1.platformdata = RAW_2_mass(patient1.platform)
+#patient1.COP = mass_2_COP(patient1.platformdata)
+patient1.COP = []
 
-fig1_max_platform = graph_platform(patient1.EMG_max_values, patient1.COP, platform = True)
-fig2_RMS = graph_RMS(patient1.EMG_RMS)
-fig3_normalization = graph_normalizedRMS(patient1.EMG_normalization)
+#fig1_max_platform = graph_platform(patient1.EMG_max_values, patient1.COP,"F_Rheumatism_Arthritis", platform = False)
+#fig2_RMS = graph_RMS(patient1.EMG_RMS, "MJ_Lupus")
+#fig3_normalization = graph_normalizedRMS(patient1.EMG_normalization, "MJ_Lupus")
 
 
 '''
--- tools/plot.py
+++ tools/plot.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.image as mpimg
+from matplotlib.backends.backend_pdf import PdfPages
 
 
-def graph_platform(max_values, platform_COP, platform = False):
+def graph_platform(max_values, platform_COP,description, platform = False):
     ind = np.arange(2)
-    width = 0.5
-    error_config = {'ecolor': '0.3', 'capthick': 0.5}
+    width = 0.3
+    error_config = {'ecolor': '1', 'capthick': 0.5}
+    pp = PdfPages('Platform_'+str(description)+'.pdf')
 
+    l = 0
     for i in max_values:
-        fig = plt.figure()
-        fig.suptitle(str(i), fontsize=20)
+        fig = plt.figure(l)
+        l = l+1
+        fig.suptitle(str(i) + "\n"+ str(description), fontsize=15)
 
         plot1 = plt.subplot2grid((3, 3), (0, 0))
         plt.bar(ind, [max_values[i][0],max_values[i][2]], width, error_kw=error_config)
@@ -25,15 +29,15 @@ def graph_platform(max_values, platform_COP, platform = False):
         plot2.set_title("Maximum for for each muscle on the right side\n Front", fontsize=10)
 
         if platform == True:
-            plot3 = plt.subplot2grid((3, 3), (1, 2), colspan=2, rowspan=2)
+            plot3 = plt.subplot2grid((3, 3), (1, 2), colspan=5, rowspan=5)
             img = mpimg.imread("tools/forcePlatform.png")
             plt.imshow(img, zorder=0, extent=[-225 - 12, +225 + 12, -225 - 12, +225 + 12])
             plt.plot(platform_COP[i]["COP_X"], platform_COP[i]["COP_Y"], color='yellow')
             plt.xlim([-225 - 12, 225 + 12])
             plt.ylim([-225 - 12, 225 + 12])
-            plt.xlabel("Trajectory of COP on X axes", fontsize=11)
-            plt.ylabel("Trajectory of COP on Y axes", fontsize=11)
-            plot3.set_title("COP trajectory", fontsize=15)
+            plt.xlabel("Trajectory of COP on X axes", fontsize=10)
+            plt.ylabel("Trajectory of COP on Y axes", fontsize=10)
+            plot3.set_title("COP trajectory", fontsize=10)
 
         plot4 = plt.subplot2grid((3, 3), (2, 0))
         plt.bar(ind, [max_values[i][4], max_values[i][6]], width, error_kw=error_config)
@@ -48,20 +52,24 @@ def graph_platform(max_values, platform_COP, platform = False):
         plot5.set_title("Maximum for for each muscle on the right side\n Back", fontsize=10)
 
         if platform == True:
-            plt.subplots_adjust(top = 0.72, bottom = 0.14, left = 0.15,right=0.91, wspace=0.56, hspace=0.03)
+            plt.subplots_adjust(top = 0.84, bottom = 0.08, left = 0.06,right=0.93, wspace=0.16, hspace=0.00)
         else:
             plt.subplots_adjust(top=0.75, bottom=0.11, left=0.30, right=0.98, wspace=0.47, hspace=0.01)
 
         plt.show()
+        pp.savefig(fig)
+    pp.close()
 
-def graph_normalizedRMS (test):
+def graph_normalizedRMS (test, description):
+    pp = PdfPages('Normalized_RMS_' + str(description) + '.pdf')
     for j in range(0, 8):
         for i in test:
+            fig = plt.figure(j)
             plt.plot(test[i][:, j], label=str(i))
-            plt.legend(bbox_to_anchor=(0.9, 1.1), loc=2, borderaxespad=0.)
+            plt.legend(bbox_to_anchor=(1.0, 1.0), loc=2, borderaxespad=0., fontsize = 10)
 
             plt.xlabel("Time in miliseconds", fontsize=7)
-            plt.suptitle("Normalized RMS", fontsize=24)
+            plt.suptitle("Normalized RMS\n" + str(description), fontsize=24)
             if j == 0:
                 plt.ylabel("Normalized RMS\n Rectus Abdominis_L")
             if j == 1:
@@ -78,16 +86,18 @@ def graph_normalizedRMS (test):
                 plt.ylabel("Normalized RMS\n Multifundus_L")
             if j == 7:
                 plt.ylabel("Normalized RMS\n Multifundus_R")
+        fig.tight_layout(pad=5.0, w_pad=1.0, h_pad=1.0)
         plt.show()
+        pp.savefig(fig)
+    pp.close()
 
-def graph_RMS(test):
+def graph_RMS(test, description):
+    pp = PdfPages('RMS_' + str(description) + '.pdf')
     for j in range(0, 8):
         for i in test:
+            fig = plt.figure(j)
             plt.plot(test[i][:,j], label= str(i))
-            plt.legend(bbox_to_anchor=(0.9, 1.1), loc=2, borderaxespad=0.)
-
-            #red_patch = mpatches.Patch(color='red', label='The red data')
-            #plt.legend(handles=[red_patch])
+            plt.legend(bbox_to_anchor=(1.0, 1.0), loc=2, borderaxespad=0., fontsize = 10)
             plt.xlabel("Time in miliseconds", fontsize=7)
             if j == 0:
                 plt.ylabel("RMS of EMG\n Rectus Abdominis_L")
@@ -105,6 +115,9 @@ def graph_RMS(test):
                 plt.ylabel("RMS of EMG\n Multifundus_L")
             if j == 7:
                 plt.ylabel("RMS of EMG\n Multifundus_R")
-            plt.suptitle("RMS", fontsize=24)
-        plt.show()
+            plt.suptitle("RMS\n" + str(description), fontsize=24)
 
+        fig.tight_layout(pad=5.0, w_pad=1.0, h_pad=1.0)
+        plt.show()
+        pp.savefig(fig)
+    pp.close()
