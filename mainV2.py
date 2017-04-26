59724e7a9e2bd6500ccd6d314e55cd038a1cea2a
Rita <arc.mendes@campus.fct.unl.pt>
2017-04-26 18:27:04 +0100

    kkk

--- mainV2.py
+++ mainV2.py
@@ -23,48 +23,51 @@ patient1.static_normalization, patient1.static_max_values = norm_whole_segment(p
 patient1.EMG_normalization, patient1.EMG_max_values = norm_whole_segment(patient1.EMG_RMS, patient1.RMS_max)
 print '\033[93m' + "NORM_END" + '\033[0m'
 
-patient1.platformdata = RAW_2_mass(patient1.platform)
-patient1.COP = mass_2_COP(patient1.platformdata)
-#patient1.COP = []
-print '\033[93m' + "COP_END" + '\033[0m'
-
-patient1.velocity, patient1.mean, patient1.acelaration = velocity_COP(patient1.COP)
-print '\033[93m' + "VELOCITY_END" + '\033[0m'
-
-patient1.trajec = trajectory(patient1.COP)
-print '\033[93m' + "TRAJ_END" + '\033[0m'
-
-patient1.c= coherence(patient1.COP, patient1.EMG_normalization)
-print '\033[93m' + "COHERENCY_END" + '\033[0m'
-
-patient1.RMS_velocity = RMS_velocity_whole_segment(patient1.velocity)
-print '\033[93m' + "V_RMS_END" + '\033[0m'
-
-patient1.RMS_acel = RMS_velocity_whole_segment(patient1.acelaration)
-print '\033[93m' + "A_RMS_END" + '\033[0m'
-
-patient1.acel_norm = normalization_COP(patient1.RMS_acel)
-print '\033[93m' + "A_NORM_END" + '\033[0m'
-
-patient1.v_norm = normalization_COP(patient1.RMS_velocity)
-print '\033[93m' + "V_NORM_END" + '\033[0m'
-
-patient1.COP_norm = normalization_COP(patient1.COP)
-print '\033[93m' + "COP_NORM_END" + '\033[0m'
-
-patient1.corr_RL = RL_muscles_COP(patient1.COP, patient1.v_norm, patient1.acel_norm, patient1.EMG_normalization)
-print '\033[93m' + "RLCORR_END" + '\033[0m'
-
-patient1.corr_FB = FB_muscles_COP(patient1.COP, patient1.v_norm, patient1.acel_norm, patient1.EMG_normalization)
-print '\033[93m' + "FBCORR_END" + '\033[0m'
-
-patient1.corr_FB_cross = FB_muscles_COP_Cross(patient1.COP, patient1.EMG_normalization)
-print '\033[93m' + "FBCORR_CROSS_END" + '\033[0m'
+# patient1.platformdata = RAW_2_mass(patient1.platform)
+# patient1.COP = mass_2_COP(patient1.platformdata)
+# #patient1.COP = []
+# print '\033[93m' + "COP_END" + '\033[0m'
+#
+# patient1.velocity, patient1.mean, patient1.acelaration = velocity_COP(patient1.COP)
+# print '\033[93m' + "VELOCITY_END" + '\033[0m'
+#
+# patient1.trajec = trajectory(patient1.COP)
+# print '\033[93m' + "TRAJ_END" + '\033[0m'
+#
+# patient1.c= coherence(patient1.COP, patient1.EMG_normalization)
+# print '\033[93m' + "COHERENCY_END" + '\033[0m'
+#
+# patient1.RMS_velocity = RMS_velocity_whole_segment(patient1.velocity)
+# print '\033[93m' + "V_RMS_END" + '\033[0m'
+#
+# patient1.RMS_acel = RMS_velocity_whole_segment(patient1.acelaration)
+# print '\033[93m' + "A_RMS_END" + '\033[0m'
+#
+# patient1.acel_norm = normalization_COP(patient1.RMS_acel)
+# print '\033[93m' + "A_NORM_END" + '\033[0m'
+#
+# patient1.v_norm = normalization_COP(patient1.RMS_velocity)
+# print '\033[93m' + "V_NORM_END" + '\033[0m'
+#
+# patient1.COP_norm = normalization_COP(patient1.COP)
+# print '\033[93m' + "COP_NORM_END" + '\033[0m'
+#
+# patient1.corr_RL = RL_muscles_COP(patient1.COP, patient1.v_norm, patient1.acel_norm, patient1.EMG_normalization)
+# print '\033[93m' + "RLCORR_END" + '\033[0m'
+#
+# patient1.corr_FB = FB_muscles_COP(patient1.COP, patient1.v_norm, patient1.acel_norm, patient1.EMG_normalization)
+# print '\033[93m' + "FBCORR_END" + '\033[0m'
+#
+# patient1.corr_FB_cross = FB_muscles_COP_Cross(patient1.COP, patient1.EMG_normalization)
+# print '\033[93m' + "FBCORR_CROSS_END" + '\033[0m'
 
 ##Creating database
 
 create_database()
-fill_parameters()
+personal_data(file)
+parameters(patient1.EMG_max_values)
+
+
 
 ##Plot figures
 
--- tools/database_excel.py
+++ tools/database_excel.py
@@ -1,11 +1,77 @@
 from tools import *
 import xlsxwriter
-import openpyxl
-import xlrd, xlwt
+from openpyxl.styles import Alignment
+import h5py
+from openpyxl.styles import Color, PatternFill, Font, Border
+from openpyxl import load_workbook
+from xlrd import open_workbook
 
 def create_database():
     workbook = xlsxwriter.Workbook('C:/Users/Rita/PycharmProjects/tonusMuscular/Excel_database/demo.xlsx')
 
-def fill_parameters():
-    wb = xlrd.open_workbook('C:/Users/Rita/PycharmProjects/tonusMuscular/Excel_database/demo.xlsx')
-    worksheet = wb.add_sheet('Patient1_Healthy')
+def personal_data(file_name):
+    file = h5py.File(file_name, 'r')
+
+    wb2 = load_workbook('C:/Users/Rita/PycharmProjects/tonusMuscular/Excel_database/demo.xlsx')
+    sheet1 = wb2.create_sheet('Patient1_Healthy')
+
+    fill = PatternFill(start_color='BCC2BC', end_color='BCC2BC', fill_type='solid')
+
+    A1 = sheet1.cell('A1')
+    A1.value = "Gender"
+    A1.font = A1.font.copy(bold=True)
+    A1.fill = fill
+
+    A2 = sheet1.cell('A2')
+    A2.value = "Age"
+    A2.font = A2.font.copy(bold=True)
+    A2.fill = fill
+
+    A3 = sheet1.cell('A3')
+    A3.value = "Condition"
+    A3.font = A3.font.copy(bold=True)
+    A3.fill = fill
+
+    A4 = sheet1.cell('A4')
+    A4.value = "Director's Hand"
+    A4.font = A4.font.copy(bold=True)
+    A4.fill = fill
+
+
+
+    B1 = sheet1.cell('B1')
+    B1.value = file.attrs.values()[1]
+    B1.alignment = Alignment(horizontal='center', vertical='center')
+
+    B2 = sheet1.cell('B2')
+    B2.value = file.attrs.values()[2]
+    B2.alignment = Alignment(horizontal='center', vertical='center')
+
+    B3 = sheet1.cell('B3')
+    B3.value = file.attrs.values()[3]
+    B3.alignment = Alignment(horizontal='center', vertical='center')
+
+    B4 = sheet1.cell('B4')
+    B4.value = "Right"
+    B4.alignment = Alignment(horizontal='center', vertical='center')
+
+
+    wb2.save('C:/Users/Rita/PycharmProjects/tonusMuscular/Excel_database/demo.xlsx')
+
+
+def parameters(EMG_values):
+    wb2 = load_workbook('C:/Users/Rita/PycharmProjects/tonusMuscular/Excel_database/demo.xlsx')
+    sheet1 = wb2.get_sheet_by_name('Patient1_Healthy')
+    count = 6
+    for i in EMG_values:
+        title = sheet1.cell('B' + str(count))
+        title.value = "Max values of each muscle - " + str(i)
+        title.font = title.font.copy(bold=True)
+        for n in range(0,8):
+
+
+        count = count + 13
+    wb2.save('C:/Users/Rita/PycharmProjects/tonusMuscular/Excel_database/demo.xlsx')
+
+
+
